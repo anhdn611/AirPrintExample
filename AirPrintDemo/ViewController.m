5e811d5e8b4f087015ebf5baab00c6473a922d77
Dao Ngoc Anh <anh.dn@citigo.net>
2016-09-17 13:57:58 +0700

    First look AirPrint

--- AirPrintDemo/ViewController.m
+++ AirPrintDemo/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIPrinter *selectedPrinter;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+ 
 @end
 
 @implementation ViewController
@@ -17,6 +20,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+   
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +29,44 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)printTapped:(id)sender {
+    [self print];
+}
+
+- (void)showPrintPicker {
+    UIPrinterPickerController *printPicker = [UIPrinterPickerController printerPickerControllerWithInitiallySelectedPrinter:self.selectedPrinter];
+    self.selectedPrinter = printPicker.selectedPrinter;
+[printPicker presentFromRect:self.button.frame inView:self.button animated:YES completionHandler:^(UIPrinterPickerController * _Nonnull printerPickerController, BOOL userDidSelect, NSError * _Nullable error) {
+    self.selectedPrinter = printerPickerController.selectedPrinter;
+    [self print];
+}];
+}
+
+- (void)print
+{
+    if (!self.selectedPrinter) {
+        [self showPrintPicker];
+        return;
+    }
+    
+    UIPrintInteractionController *pc = [UIPrintInteractionController
+                                        sharedPrintController];
+    UIPrintInfo *printInfo = [UIPrintInfo printInfo];
+    printInfo.outputType = UIPrintInfoOutputGeneral;
+    printInfo.jobName = @"KiotViet";
+    pc.printInfo = printInfo;
+    pc.showsPageRange = YES;
+    
+    NSString *printString = [NSString stringWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"InvoiceSimple" ofType:@"html"] encoding:NSUTF8StringEncoding error:nil];
+    
+    UIMarkupTextPrintFormatter *markup = [[UIMarkupTextPrintFormatter alloc] initWithMarkupText:printString];
+    
+    pc.printInfo = printInfo;
+    pc.printFormatter = markup;
+    
+    [pc printToPrinter:self.selectedPrinter completionHandler:^(UIPrintInteractionController * _Nonnull printInteractionController, BOOL completed, NSError * _Nullable error) {
+        
+    }];
+
+}
 @end
