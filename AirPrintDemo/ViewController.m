18fe65334fa55ebb8b696816be233a317875de2a
Dao Ngoc Anh <anh.dn@citigo.net>
2016-09-19 15:01:13 +0700

    add default printer controller

--- AirPrintDemo/ViewController.m
+++ AirPrintDemo/ViewController.m
@@ -8,11 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIPrintInteractionControllerDelegate>
 
 @property (nonatomic, strong) UIPrinter *selectedPrinter;
 @property (weak, nonatomic) IBOutlet UIButton *button;
- 
+@property (weak, nonatomic) IBOutlet UIView *printerView;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
 @end
 
 @implementation ViewController
@@ -20,7 +23,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-   
+   NSString *printString = [NSString stringWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"InvoiceSimple" ofType:@"html"] encoding:NSUTF8StringEncoding error:nil];
+    NSAttributedString *attributedString = [[NSAttributedString alloc]
+                                            initWithData: [printString dataUsingEncoding:NSUnicodeStringEncoding]
+                                            options: @{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType }
+                                            documentAttributes: nil
+                                            error: nil
+                                            ];
+    self.textView.attributedText = attributedString;
+    [self.webView loadHTMLString:printString baseURL:nil];
+}
+- (IBAction)defaultPrint:(id)sender {
+    UIPrintInteractionController *pc = [UIPrintInteractionController
+                                        sharedPrintController];
+    UIPrintInfo *printInfo = [UIPrintInfo printInfo];
+    printInfo.outputType = UIPrintInfoOutputGrayscale;
+    printInfo.jobName = @"KiotViet";
+    printInfo.orientation = UIPrintInfoOrientationPortrait;
+    printInfo.duplex = UIPrintInfoDuplexNone;
+    pc.showsPageRange = YES;
+    
+    NSString *printString = [NSString stringWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"InvoiceSimple" ofType:@"html"] encoding:NSUTF8StringEncoding error:nil];
+    
+    UIMarkupTextPrintFormatter *markup = [[UIMarkupTextPrintFormatter alloc] initWithMarkupText:printString];
+    pc.delegate = self;
+    pc.printInfo = printInfo;
+    pc.printFormatter = markup;
+    [pc presentAnimated:YES completionHandler:^(UIPrintInteractionController * _Nonnull printInteractionController, BOOL completed, NSError * _Nullable error) {
+        [printInteractionController dismissAnimated:YES];
+    }];
+}
+
+- (void)printInteractionControllerWillPresentPrinterOptions:(UIPrintInteractionController *)printInteractionController {
+    
+}
+- (void)printInteractionControllerDidPresentPrinterOptions:(UIPrintInteractionController *)printInteractionController {
+    
+}
+- (void)printInteractionControllerWillDismissPrinterOptions:(UIPrintInteractionController *)printInteractionController {
+    
+}
+- (void)printInteractionControllerDidDismissPrinterOptions:(UIPrintInteractionController *)printInteractionController {
+    
+}
+
+- (void)printInteractionControllerWillStartJob:(UIPrintInteractionController *)printInteractionController {
+    
+}
+- (void)printInteractionControllerDidFinishJob:(UIPrintInteractionController *)printInteractionController {
     
 }
 
@@ -30,20 +80,21 @@
 }
 
 - (IBAction)printTapped:(id)sender {
-    [self print];
+    [self printFromWebView];
 }
 
 - (void)showPrintPicker {
     UIPrinterPickerController *printPicker = [UIPrinterPickerController printerPickerControllerWithInitiallySelectedPrinter:self.selectedPrinter];
     self.selectedPrinter = printPicker.selectedPrinter;
-[printPicker presentFromRect:self.button.frame inView:self.button animated:YES completionHandler:^(UIPrinterPickerController * _Nonnull printerPickerController, BOOL userDidSelect, NSError * _Nullable error) {
-    self.selectedPrinter = printerPickerController.selectedPrinter;
-    [self print];
-}];
+    [printPicker presentFromRect:self.button.frame inView:self.printerView animated:YES completionHandler:^(UIPrinterPickerController * _Nonnull printerPickerController, BOOL userDidSelect, NSError * _Nullable error) {
+        self.selectedPrinter = printerPickerController.selectedPrinter;
+        [self printFromWebView];
+    }];
 }
 
-- (void)print
+- (void)printFromWebView
 {
+    
     if (!self.selectedPrinter) {
         [self showPrintPicker];
         return;
@@ -52,21 +103,18 @@
     UIPrintInteractionController *pc = [UIPrintInteractionController
                                         sharedPrintController];
     UIPrintInfo *printInfo = [UIPrintInfo printInfo];
-    printInfo.outputType = UIPrintInfoOutputGeneral;
+    printInfo.outputType = UIPrintInfoOutputGrayscale;
     printInfo.jobName = @"KiotViet";
     pc.printInfo = printInfo;
+    
     pc.showsPageRange = YES;
     
-    NSString *printString = [NSString stringWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"InvoiceSimple" ofType:@"html"] encoding:NSUTF8StringEncoding error:nil];
-    
-    UIMarkupTextPrintFormatter *markup = [[UIMarkupTextPrintFormatter alloc] initWithMarkupText:printString];
-    
     pc.printInfo = printInfo;
-    pc.printFormatter = markup;
+    UIPrintFormatter *formater = [self.webView viewPrintFormatter];
+    pc.printFormatter = formater;
     
     [pc printToPrinter:self.selectedPrinter completionHandler:^(UIPrintInteractionController * _Nonnull printInteractionController, BOOL completed, NSError * _Nullable error) {
-        
     }];
-
 }
+
 @end
